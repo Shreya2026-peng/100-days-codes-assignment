d2be6b2bc7b659e035cd49b25f85ccf6c830a76d
Shreya2026-peng <shreya.rani.2022@gmail.com>
2026-08-21 00:25:59 +0530

    Update and rename code1.c to day1code1.c

---
+++ day1code1.c
@@ -0,0 +1,13 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+#include <stdio.h>
+int main()
+{
+  int a,b,c;
+printf("enter the two integers:");
+scanf("%d %d",&a,&b);
+sum = a+b;
+printf("sum = %d\n",sum);
+return 0;
+}
+  
