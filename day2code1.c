8e7c38650569dec9a1d738dcc5c3f97995565909
Shreya2026-peng <shreya.rani.2022@gmail.com>
2026-08-24 17:05:03 +0530

    Create day2code1.c

---
+++ day2code1.c
@@ -0,0 +1,13 @@
+//Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+#include <stdio.h>
+int main()
+{
+  int length,breadth,area,perimeter;
+printf("enter the length and breadth:");
+scanf("%d %d, &length, &breadth");
+area = length*breadth;
+perimeter = 2*(length+breadth);
+printf("area = %d\n",area);
+printf("perimeter = %d\n",perimeter);
+return 0;
+}
