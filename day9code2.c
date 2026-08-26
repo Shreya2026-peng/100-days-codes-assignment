1da039189386c6196ba43f7872ea0e047344e4c2
Shreya2026-peng <shreya.rani.2022@gmail.com>
2026-08-26 14:34:49 +0530

    Update day9code2.c

--- day9code2.c
+++ day9code2.c
@@ -8,3 +8,20 @@ below 60: Grade F.
 #include <stdio.h>
   int main()
   {
+float percentage;
+printf("enter percentage:")
+scanf("%d", &percentage);
+if (percentage>=90 && percentage<= 100)
+  print("grade A\n");
+else if (percentage >= 80)
+  printf("grade B\n");
+else if (percentage >=70)
+  printf("grade C\n");
+else if (percentage >=60)
+  printf("grade D\n");
+else if (percentage >=0)
+  printf("grade F\n);
+else
+    printf("invalid percentage\n")
+    return 0;
+}
