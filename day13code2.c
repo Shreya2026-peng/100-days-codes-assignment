49e33adffa1a443528b247be78232f1b18e90e9d
Shreya2026-peng <shreya.rani.2022@gmail.com>
2026-08-27 10:41:46 +0530

    Create day13code2.c

---
+++ day13code2.c
@@ -0,0 +1,18 @@
+//Q26: Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i;
+
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+  {
+  printf("%d\n ", i);
+}
+
+return 0;
+}
